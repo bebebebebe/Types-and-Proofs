dcf0b5fc03694dda0786a0cfcc5d1f6896fb3e6f
Be Birchall <bebebebebe@users.noreply.github.com>
2024-10-23 16:39:15 -0400

    dpll satisfiability algorithm

--- bin/dpll_sat.ml
+++ bin/dpll_sat.ml
@@ -10,7 +10,7 @@ A cnf [] corresponds to true
 A clause [] corresponds to false
 *)
 
-(* Simple test case - example cnf symbolizes: (x1 v ~x2) & x3 *)
+(* Simple test case - example cnf symbolizes: (~x1 v ~x2) & x3 *)
 let p = [[(true, 1); (false, 2)]; [(true, 3)]]
 
 (* Define mem, map, fliter without using built in list primitives *)
@@ -40,3 +40,62 @@ let rec subst_cnf x is_true p =
       (fun y -> list_filter (fun z -> z <> (true, x)) y)    (* remove (true, x) literals from remaining clauses *)
       (list_filter (fun y -> not(list_mem (false, x) y)) p) (* remove clauses with (false, x) *)
 
+let rec dpll_simple p =
+  match p with
+  | [] -> true
+  | p' when list_mem [] p' -> false
+  | _ -> dpll_simple (subst_cnf (snd (List.hd(List.hd p))) true p) || dpll_simple (subst_cnf (snd (List.hd(List.hd p))) false p)
+
+exception Not_found
+
+(* return literal if a clause consisting of one literal exists *)
+let rec unit = function
+| [] -> raise Not_found
+| c :: t -> if List.length(c) = 1 then List.hd(c) else unit t
+
+
+(* a literal is pure if the variable always occurs with the same polarity (true or false) in every clause
+  pure finds a pure literal in a formula if it exists
+*)
+
+(* is a given literal x in any clause of the cnf? *)
+let rec has_literal x = function
+| [] -> false
+| c :: t -> list_mem x c || has_literal x t
+
+(* remove x, not x literals from cnf *)
+let remove_literals x p =
+  list_map (fun c -> list_filter (fun (_, v) -> v != x) c) p
+
+let rec pure = function
+| [] -> raise Not_found
+| [] :: t -> pure t
+| ((b, v) :: c) :: t->
+  if has_literal (not b, v) (c :: t) then pure (remove_literals v (c :: t)) (* TODO: reduce steps *)
+  else (b, v)
+
+(* test case *)
+let q = [[(true, 1); (true, 2)]; [(false, 3)]]
+
+(* Improve on dpll_simple above as follows:
+if the formula is empty then it returns true,
+if the formula contains the empty clause then it returns false,
+if the formula contains a unitary clause then it replaces the corresponding variable by the only possible value,
+if the formula contains a pure literal then it replaces the corresponding variable by the value which preserves satisfiability,
+otherwise, it splits on an arbitrary variable.
+*)
+
+let rec dpll p =
+  match p with
+  | [] -> true
+  | p' when list_mem [] p' -> false
+  | _ ->
+    try (match unit p with
+    | (true, v) -> dpll (subst_cnf v true p)
+    | (false, v) -> dpll (subst_cnf v false p))
+  with Not_found ->
+    try (match pure p with
+    | (true, v) -> dpll (subst_cnf v true p)
+    | (false, v) -> dpll (subst_cnf v false p))
+  with Not_found ->
+    dpll (subst_cnf (snd (List.hd(List.hd p))) true p) || dpll (subst_cnf (snd (List.hd(List.hd p))) false p)
